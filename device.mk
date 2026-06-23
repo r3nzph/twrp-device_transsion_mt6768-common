78264c54e1be840a81a99e983614bfedac1f1c57
r3nzph <abarracosorenznino@gmail.com>
2026-06-24 02:49:38 +0800

    Add Bootctrl packages for MTK implementation

--- device.mk
+++ device.mk
@@ -37,6 +37,11 @@ PRODUCT_PACKAGES += \
     fastbootd \
     android.hardware.fastboot@1.0-impl-mock
 
+# Bootctrl (KEEP MTK IMPLEMENTATION)
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
 # Keystore2
 PRODUCT_PACKAGES += \
     android.system.keystore2
